61730c9807917b058d20e3e9cd07d352aa1fc2e2
emvau <matthias.vongerichten@gmail.com>
2013-08-08 10:39:30 +0200

    Navigationskonzept 1.0, Literatur hinzugefügt

--- Website/reiseziele/amsterdam.php
+++ Website/reiseziele/amsterdam.php
@@ -1,5 +1,6 @@
 ﻿<?php 
 $title = "Amsterdam";
+$desc = "4-Tage Busreise nach Amsterdam - Superior Hotel - Grachtenfahrten";
 include($_SERVER['DOCUMENT_ROOT'].'/int02/header.php'); 
 ?>
 <div id="inner-wrapper">
@@ -8,11 +9,11 @@ include($_SERVER['DOCUMENT_ROOT'].'/int02/header.php');
 			<a>Home</a> > <a>Reiseziele</a> > <a>Niederlande</a>
 		</div>
 		<h1>Amsterdam</h1>
-		<img src="/int02/img/amsterdam1.jpg" />
+		<img alt ="Flussufer in Amsterdam" src="/int02/img/amsterdam1.jpg" />
 		<p>Die Hafenstadt Amsterdam (niederländisch  Amsterdam?/i) ist die Hauptstadt und einwohnerstärkste Stadt der Niederlande. Amsterdam hat 799.345 Einwohner (Stand: 1. Januar 2013)[2] und als Agglomeration Groot-Amsterdam etwa 1,3 Millionen (August 2012)[3].</p>
 		
 		<p>Im Gro?raum Amsterdam, der den nördlichen Teil des niederländischen Verdichtungsraumes Randstad ausmacht, leben etwa 2,4 Millionen Menschen (2012)[4]. Auch wenn sich der Regierungssitz des Landes sowie die Königsresidenz im 60 Kilometer entfernten Den Haag befinden, ist Amsterdam seit 1983 gemäß niederländischer Verfassung die Hauptstadt der Niederlande.[5] Amsterdam liegt in der Provinz Nordholland, an der Mündung der Amstel und des IJ in das IJsselmeer. Amsterdam ist durch den Noordzeekanaal mit der Nordsee verbunden und durch die zahlreichen Grachten weltberühmt.</p>
-		<img src="/int02/img/amsterdam2.jpg" />
+		<img alt ="Fluss in Amsterdam" src="/int02/img/amsterdam2.jpg" />
 		<h2>Tag 1</h2>
 		<p>Die Fahrt nach Amsterdam startet gegen Abend, die genaue Abfahrtszeit variiert je nach Abfahrtsort</p>
 		<h2>Tag 2</h2>
@@ -26,6 +27,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/int02/header.php');
 		<h2>Tag 4</h2>
 		<p>Gegen 9:30 Uhr, nach einem gemütlichen Frühstück, checken wir aus dem Hotel aus und machen uns auf den Weg nach Volendam (10€ p.P.), ein kleines Dorf direkt am Ijsselmeer. Wir machen Halt bei einem auf dem Weg gelegenen "Klompenmaker", der die typischen holländischen Holzschuhe herstellt. </p>
 		<p>Gegen 13:30 Uhr brechen wir auf in Richtung Deutschland. Gegen Nachmittag erreichen wir NRW, entferntere Abfahrtsorte erreichen wir entsprechend später.</p>
+		<p>Beispieltext von http://www.solegro.de/staedtereisen/amsterdam-bus/3-tage-amsterdam-komforthotels<br />Bilder von http://www.stockvault.net/</p>
 	</div>
 	<div id="sidebar" class="clearfix">
 		<div class="sidebar-topic">Reisezeit<span class="corner-right"></span></div>
@@ -65,7 +67,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/int02/header.php');
 		</ul>
 		<div class="sidebar-topic">Preis<span class="corner-right"></span></div>
 		<p>350€ pro Person</p>
-		<div id="booking" class="sidebar-topic"><a>Buchen</a><span class="corner-right"></span></div>
+		<div id="booking" class="sidebar-topic"><a>Jetzt buchen</a><span class="corner-right"></span></div>
 	</div>
 	<div class="clear"></div>
 
