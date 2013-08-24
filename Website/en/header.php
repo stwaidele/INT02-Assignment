aeff99a22c84fb23a3ca71e4d67a3f66648dc870
emvau <matthias.vongerichten@gmail.com>
2013-08-24 11:46:50 +0200

    +Mehrsprachigkeit

---
+++ Website/en/header.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>DeinBus - <?php echo $title; ?></title>
+
+<!-- Meta Tags -->
+<meta charset="utf-8" />
+<meta name="description" content="<?php echo $desc; ?>" />
+	<!-- Setup of the viewport for mobile devices -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- Stylesheet & Favicon -->
+<link rel="stylesheet" type="text/css" href="/style.css" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<div id="wrap">
+<div id="header">
+	<div id="social-bar">
+		<div class="fb-like-box" data-href="https://www.facebook.com/DeinBus" data-width="200" data-show-faces="false" data-header="false" data-stream="false" data-show-border="false"></div>
+		<g:plusone href="http://int02.10100.de" size="Tall"></g:plusone>
+	</div>
+	<div id="logo" class="clearfix">
+		<div id="main">
+			<a href="/en" title="Back to home"><img id="logo" 
+            alt="DeinBus.de Logo" 
+            src="/img/deinbus_transparent.png"/></a>
+		</div>
+	</div>  
+	<div id="nav">
+		<ul>
+        	<li><a href="/en" title="Home">Home</a></li>
+			<li><a href="/en/destinations" title="Overview of the offered destinations">Destinations</a></li>
+            <li><a href="/en/buses" title="With our buses you travel with comfort">The Buses</a></li>
+			<li><a href="/en/aboutus" title="Get to know our staff">About us</a></li>
+			<li><a href="/en/contact" title="We will be happy to answer all your questions">Contact</a></li>
+		<ul>
+		<span class="corner-left"></span>
+		<span class="corner-right"></span>
+	</div>
+</div>
---
+++ Website/en/index.php
@@ -0,0 +1,29 @@
+﻿<?php 
+$title="Home";
+$desc="Europe-wide City trips - Comfort Hotels - Premium Class Buses";
+include($_SERVER['DOCUMENT_ROOT'].'/en/header.php'); 
+?>
+<div id="inner-wrapper" class="clearfix">
+
+	<div id="content">
+		<div id="breadcrump">
+			Home
+		</div>
+		<h1>Welcome</h1>
+        <img src="/img/start.jpg" alt="Titelbild" class="titel" />
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+	</div>
+	<div id="sidebar" class="clearfix">
+		<div class="sidebar-topic">Latest Offers<span class="corner-right"></span></div>
+		<ul>
+			<li><a href="/en/reiseziele/amsterdam" title="Käse, Tulpen, Weed">Amsterdam</a></li>
+		</ul>
+	</div>
+	<div class="clear"></div>
+</div>
+
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/en/footer.php'); ?>
+
