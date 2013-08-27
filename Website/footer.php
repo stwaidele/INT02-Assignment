cc08941912a510d56034cec68eb38c7e05476e09
Stefan Waidele <Stefan@Waidele.info>
2013-08-27 12:17:58 +0200

    Pure CSS Dropdown Menu

--- Website/footer.php
+++ Website/footer.php
@@ -1,10 +1,9 @@
 ﻿<div id="footer">
-	<div id="right">
-		<a>Impressum</a> | <a>AGB</a> | <a>Sitemap</a> | © Copyright DeinBus 2013
-	</div>
-	<a title="Home" href="/en"><img alt="english flag" src="/img/english.png" /></a>
-	<a title="Startseite" href="/"><img alt="german flag" src="/img/german.png" /></a>
+	<a>Impressum</a> | <a>AGB</a> | <a>Sitemap</a> | © Copyright DeinBus 2013
 </div>
 </div>
+
+
+
 </body>
 </html>
